c49bb2e88ea8b062d5d00c85b41ceb46891a86d8
Adom Hartell <adomhartell@gmail.com>
2017-04-11 16:38:23 -0700

    complete static_array

--- project1/lib/static_array.rb
+++ project1/lib/static_array.rb
@@ -1,14 +1,20 @@
 # This class just dumbs down a regular Array to be staticly sized.
 class StaticArray
   def initialize(length)
+    @length = length
+    @store = Array.new(length)
   end
 
   # O(1)
   def [](index)
+    raise "index out of range" if index > @length - 1
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    raise "index out of range" if index > @length - 1
+    @store[index] = value
   end
 
   protected
