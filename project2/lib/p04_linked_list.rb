d28af205abb7e37e0f5b7da36ee9e182eaccc911
Adom Hartell <adomhartell@gmail.com>
2017-04-12 16:51:59 -0700

    complete linked list

--- project2/lib/p04_linked_list.rb
+++ project2/lib/p04_linked_list.rb
@@ -13,13 +13,22 @@ class Link
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    self.prev.next = self.next if self.prev
+    self.next.prev = self.prev if self.next
+    self.next = nil
+    self.prev = nil
+    self
   end
+
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +37,63 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : @head.next
   end
 
   def last
+    empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+    each { |link| return true if link.key == key }
+    false
   end
 
   def append(key, val)
+    new_link = Link.new(key, val)
+    current_last = @tail.prev
+
+    current_last.next = new_link
+    new_link.prev = current_last
+    new_link.next = @tail
+    @tail.prev = new_link
+
+    new_link
   end
 
   def update(key, val)
+    each { |link| link.val = val if link.key == key }
   end
 
   def remove(key)
+    each do |link|
+      if link.key == key
+        link.remove
+        return link.val
+      end
+    end
+
+    nil
   end
 
-  def each
+  def each(&prc)
+    current_link = @head.next
+    until current_link == @tail
+      prc.call(current_link)
+      current_link = current_link.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
