b572bd061f361ba6db5ee2ced1ab26a78fde792d
Adom Hartell <adomhartell@gmail.com>
2017-04-12 17:13:01 -0700

    complete lru cache

--- project2/lib/p06_lru_cache.rb
+++ project2/lib/p06_lru_cache.rb
@@ -2,7 +2,8 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
-  attr_reader :count
+  attr_reader :count, :max
+
   def initialize(max, prc)
     @map = HashMap.new
     @store = LinkedList.new
@@ -15,22 +16,40 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      link = @map[key]
+      update_link!(link)
+      link.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
-    "Map: " + @map.to_s + "\n" + "Store: " + @store.to_s
+    "Map: #{@map} \nStore: #{@store}"
   end
 
   private
 
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    link = @store.append(key, val)
+    @map.set(key, link)
+
+    eject! if count > max
+    val
   end
 
   def update_link!(link)
-    # suggested helper method; move a link to the end of the list
+    link.remove
+    @store.append(link.key, link.val)
   end
 
   def eject!
+    lru_key = @store.first.key
+    @store.remove(lru_key)
+    @map.delete(lru_key)
+
+    nil
   end
 end
