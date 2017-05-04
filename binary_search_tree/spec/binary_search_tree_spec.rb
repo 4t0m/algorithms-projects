324fd6c885467a0f410054e1b5a306ca3a45bacf
Adom Hartell <adomhartell@gmail.com>
2017-05-03 19:01:39 -0700

    add specs for tree#find method

--- binary_search_tree/spec/binary_search_tree_spec.rb
+++ binary_search_tree/spec/binary_search_tree_spec.rb
@@ -13,7 +13,6 @@ describe BinarySearchTree do
 		end
 	end
 
-
 	context "insertion" do
 		it "sets first inserted node as root" do
 			tree.insert(node2)
@@ -35,6 +34,35 @@ describe BinarySearchTree do
 			tree.insert(node3)
 			tree.insert(node4)
 			expect(node3.right).to eq(node4)
+		end
+	end
+
+	context "find" do
+		it "finds the value of the root element" do
+			tree.insert(node2)
+			expect(tree.find(2)).to be true
+		end
+
+		it "finds elementns on the left and right" do
+			tree.insert(node2)
+			tree.insert(node1)
+			tree.insert(node3)
+			
+			expect(tree.find(1)).to be true
+			expect(tree.find(3)).to be true
+		end
+
+		it "finds elements recursively" do
+			tree.insert(node2)
+			tree.insert(node1)
+			tree.insert(node3)
+			tree.insert(node4)
+
+			expect(tree.find(4)).to be true
+		end
+
+		it "does not find an element that has not been inserted" do
+			expect(tree.find(1)).to be false
 		end
 	end
 end
