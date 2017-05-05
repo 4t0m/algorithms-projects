8e43849646d65e7f93e9525c766d3dbc37d4546d
Adom Hartell <adomhartell@gmail.com>
2017-05-05 16:20:49 -0700

    add specs for BinarySearchTree#delete

--- binary_search_tree/spec/binary_search_tree_spec.rb
+++ binary_search_tree/spec/binary_search_tree_spec.rb
@@ -2,10 +2,12 @@ require 'binary_search_tree'
 
 describe BinarySearchTree do
 	let (:tree) { BinarySearchTree.new }
+	let (:node0) { BSTNode.new(0) }
 	let (:node1) { BSTNode.new(1) }
 	let (:node2) { BSTNode.new(2) }
 	let (:node3) { BSTNode.new(3) }
 	let (:node4) { BSTNode.new(4) }
+	let (:node5) { BSTNode.new(5) }
 
 	context "initialization" do
 		it "sets root to nil" do
@@ -78,6 +80,50 @@ describe BinarySearchTree do
 			tree.insert(node2)
 
 			expect(tree.find(1)).to be false
+		end
+	end
+
+	context "deletion" do 
+		it "deletes a node with no children" do 
+			tree.insert(node2)
+			tree.insert(node1)
+			tree.delete(1)
+
+			expect(node2.left).to be_nil
+		end
+	
+		it "deletes a node with one child" do
+			tree.insert(node3)
+			tree.insert(node2)
+			tree.insert(node1)
+			tree.delete(2)
+
+			expect(node3.left).to eq(node1)
+		end
+
+		it "deletes the root node and sets a new root" do
+			tree.insert(node2)
+			tree.insert(node1)
+			tree.delete(2)
+
+			expect(tree.root).to eq(node1)
+		end
+
+		it "deletes a node with two children and promotes children as required" do
+			tree.insert(node4)
+			tree.insert(node0)
+			tree.insert(node5)
+			tree.insert(node3)
+			tree.insert(node1)
+			tree.insert(node2)
+			tree.delete(4)
+
+			expect(node3.left).to eq(node0)
+			expect(node0.right).to eq(node1)
+		end
+
+		it "returns false when given value is not in tree" do
+			expect(tree.delete(2)).to be false
 		end
 	end
 end
